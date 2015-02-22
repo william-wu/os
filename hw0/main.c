92edca42c98a89cfd605637390eb5d3e4f084abd
William Wu <xiaoqiangwu.school@gmail.com>
2015-02-22 05:25:12 +0000

    hw1 commit

--- hw0/main.c
+++ hw0/main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <unistd.h>
 
 int main() {
-    struct rlimit lim;
-    printf("stack size: %d\n",  0);
-    printf("process limit: %d\n", 0);
-    printf("max file descriptors: %d\n", 0);
+    struct rlimit sl, pl, fl;
+	if (getrlimit(RLIMIT_STACK, &sl) == -1) {
+		printf("shit happened\n");
+		return -1;
+	}
+	printf("stack size:%lld\n", (long long int) sl.rlim_cur/1024);
+    
+	if (getrlimit(RLIMIT_NPROC, &pl) == -1) {
+		printf("shit happened\n");
+		return -1;
+	}
+	printf("process limit: %lld\n", (long long int)pl.rlim_cur);
+	
+	if (getrlimit(RLIMIT_NOFILE, &fl) == -1) {
+		printf("shit happened\n");
+		return -1;
+	}
+    printf("max file descriptors: %lld\n", (long long int)fl.rlim_cur);
 }
--- hw0/wc.c
+++ hw0/wc.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
-#include <ctype.h>
 
-void wc(FILE *ofile, FILE *infile, char *inname) {
-}
+#define IN 1
+#define OUT 0
 
-int main (int argc, char *argv[]) {
-    return 0;
+int main(int argc, char *argv[]) {
+
+	if (argc != 2)
+	{
+		printf("usage: %s filename\n", argv[0]);
+		return 0;
+	}
+
+	char *fn = argv[1];
+	FILE *fp = fopen(fn, "r");
+
+	int nl, nw, nc, c, state;
+
+	nl = nw = nc = 0;
+	state = OUT;
+	while((c = fgetc(fp)) != EOF) {
+		nc++;
+		if (c == '\n')
+			nl++;
+
+		if (c == ' ' || c == '\t' || c == '\n') {
+			state = OUT;
+		} else if (state == OUT) {
+			state = IN;
+			nw++;
+		}
+	}
+	printf("%d\t%d\t%d\n", nl, nw, nc);
 }
